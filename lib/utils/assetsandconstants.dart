1eb15bc9de0c8e99a797e45fa74592477a9b5d72
Jokerohit4 <Jokerohit4@users.noreply.github.com>
2023-07-11 19:30:35 +0530

    'origin/master'

--- lib/utils/assetsandconstants.dart
+++ lib/utils/assetsandconstants.dart
@@ -2,40 +2,40 @@
 
 
 abstract class AssetsAndConstants{
-  static const String birdIcon = "assets/bird_icon.png";
-  static const String catIcon = "assets/cat_icon.png";
-  static const String dogIcon = "assets/dog_icon.png";
-  static const String rabbitIcon = "assets/rabbit_icon.png";
-  static const String othersIcon = "assets/others_icon.png";
-  static const String ageIcon = "assets/age_icon.png";
-  static const String filterIcon = "assets/filter_icon.png";
-  static const String adoptMeIcon = "assets/adopt_me_icon.png";
-  static const String adoptMeWhiteIcon = "assets/adopt_me_white_icon.png";
-  static const String knowMeIcon = "assets/know_me_icon.png";
-  static const String moneyIcon = "assets/money_icon.png";
-  static const String purpleBgDecor = "assets/purple_bg_decor.png";
-  static const String pinkBgDecor = "assets/pink_bg_decor.png";
-  static const String blueBgDecor = "assets/pink_bg_decor.png";
-  static const String greenBgDecor = "assets/green_bg_decor.png";
-  static const String orangeBgDecor = "assets/orange_bg_decor.png";
-  static const String dogZero = "assets/dog0.png";
-  static const String dogOne = "assets/dog1.png";
-  static const String dogTwo = "assets/dog2.png";
-  static const String dogThree = "assets/dog3.png";
-  static const String dogFour= "assets/dog4.png";
-  static const String birdZero = "assets/bird0.png";
-  static const String birdOne = "assets/bird1.png";
-  static const String birdTwo = "assets/bird2.png";
-  static const String birdThree = "assets/bird3.png";
-  static const String camelZero = "assets/camel0.png";
-  static const String catZero = "assets/cat0.png";
-  static const String catOne = "assets/cat1.png";
-  static const String rabbitZero = "assets/rabbit0.png";
-  static const String rabbitOne = "assets/rabbit1.png";
+  static const String birdIcon = "assets/assets/bird_icon.png";
+  static const String catIcon = "assets/assets/cat_icon.png";
+  static const String dogIcon = "assets/assets/dog_icon.png";
+  static const String rabbitIcon = "assets/assets/rabbit_icon.png";
+  static const String othersIcon = "assets/assets/others_icon.png";
+  static const String ageIcon = "assets/assets/age_icon.png";
+  static const String filterIcon = "assets/assets/filter_icon.png";
+  static const String adoptMeIcon = "assets/assets/adopt_me_icon.png";
+  static const String adoptMeWhiteIcon = "assets/assets/adopt_me_white_icon.png";
+  static const String knowMeIcon = "assets/assets/know_me_icon.png";
+  static const String moneyIcon = "assets/assets/money_icon.png";
+  static const String purpleBgDecor = "assets/assets/purple_bg_decor.png";
+  static const String pinkBgDecor = "assets/assets/pink_bg_decor.png";
+  static const String blueBgDecor = "assets/assets/pink_bg_decor.png";
+  static const String greenBgDecor = "assets/assets/green_bg_decor.png";
+  static const String orangeBgDecor = "assets/assets/orange_bg_decor.png";
+  static const String dogZero = "assets/assets/dog0.png";
+  static const String dogOne = "assets/assets/dog1.png";
+  static const String dogTwo = "assets/assets/dog2.png";
+  static const String dogThree = "assets/assets/dog3.png";
+  static const String dogFour= "assets/assets/dog4.png";
+  static const String birdZero = "assets/assets/bird0.png";
+  static const String birdOne = "assets/assets/bird1.png";
+  static const String birdTwo = "assets/assets/bird2.png";
+  static const String birdThree = "assets/assets/bird3.png";
+  static const String camelZero = "assets/assets/camel0.png";
+  static const String catZero = "assets/assets/cat0.png";
+  static const String catOne = "assets/assets/cat1.png";
+  static const String rabbitZero = "assets/assets/rabbit0.png";
+  static const String rabbitOne = "assets/assets/rabbit1.png";
 
-  static const String pawIcon = "assets/paw_icon.png";
-  static const String locationIcon = "assets/location_icon.png";
+  static const String pawIcon = "assets/assets/paw_icon.png";
+  static const String locationIcon = "assets/assets/location_icon.png";
 
 
-  static const String confettiGif = 'assets/6ob.gif';
+  static const String confettiGif = 'assets/assets/6ob.gif';
 }
